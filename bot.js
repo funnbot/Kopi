9eadd163a1aab2256dd9283955897a904126dcca
funnbot <dillonbayless@MacBook-Pro.local>
2019-06-11 21:56:10 -0700

    lazzyyy

--- bot.js
+++ bot.js
@@ -8,47 +8,49 @@ const bot = new Client({
     fetchAllMembers: true
 });
 
-const COMMANDS = {
-    "list": {
-        help: "List the available roles.",
-        exec: (m, args) => {
-
-        }
-    },
-    "assign": {
-        help: "Assign a role",
-        exec: (m, args) => {
-
-        }
-    },
-    "remove": {
-        help: "Remove a role",
-        exec: (m, args) => {
-
-        }
-    },
-    "reload": { // SPOOK (admin check)
-        help: "Reload the settings cache.",
-        exec: (m, args) => {
-            // Special people
-            if (!settings.admins.contains(m.author.id)) return;
-        }
-    },
-    "eval": { // DOUBLE SPOOK
-        help: "Eval a scary bit of javascript",
-        exec: message => {
-            // OG VIP
-            if (message.author.id != settings.eval) return;
-        }
-    }
-}
-
+const TOKEN_REGEX = new RegExp(settings.token, "gim");
 const ROLE_REGEX = /<@\d{17,19}>/;
 let MENTION_REGEX;
 bot.on("ready", () => {
     MENTION_REGEX = new RegExp(`<@!?${bot.id}>`);
     console.log("I Am.");
 })
+
+const COMMANDS = {
+    "list": {
+        help: "List the available roles.",
+        exec: async (m, args) => {
+
+        }
+    },
+    "assign": {
+        help: "Assign a role",
+        exec: async (m, args) => {
+
+        }
+    },
+    "remove": {
+        help: "Remove a role",
+        exec: async (m, args) => {
+
+        }
+    },
+    "reload": { // SPOOK (admin check)
+        help: "Reload the settings cache.",
+        exec: async (m, args) => {
+            // Special people
+            if (!settings.admins.contains(m.author.id)) return;
+        }
+    },
+    "eval": { // DOUBLE SPOOK
+        help: "Eval a scary bit of javascript",
+        exec: async (m, args) => {
+            // OG VIP
+            if (m.author.id != settings.owner) return;
+            const code = 
+        }
+    }
+}
 
 bot.on("message", message => {
     // Stinky bots
@@ -61,9 +63,7 @@ bot.on("message", message => {
     // * NOTE: cleanContent might create errors *
     const content = message.cleanContent;
 
-    const splitBySpace = message.cleanContent.split(' ');
-    // Filter out extra spaces
-    let args = splitBySpace.filter(s => s)
+    const args = message.cleanContent.split(' ');
 
     // Prefix check
     if (!settings.prefix.length || !content.startsWith(settings.prefix)) {
@@ -71,18 +71,21 @@ bot.on("message", message => {
         if (MENTION_REGEX.test(args[0])) {
             args = args.slice(1);
         } else return; // Otherwise this is not a command.
+    } else {
+        args[0] = args[0].slice(settings.prefix.length);
     }
 
     const cmd = args.shift();
 
     if (COMMANDS.hasOwnProperty(cmd)) {
         try {
-            COMMANDS[cmd].exec(message, args.slice(1));
+            await COMMANDS[cmd].exec(message, args.slice(1));
         } catch (e) {
             console.error(`Command ${cmd} error: ${e}`);
         }
     }
 });
 
-bot.login(settings.token);
+bot.login(settings.token)
+    .catch(console.error);
 
